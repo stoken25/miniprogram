548dc95c30158abfceb029fb16750a53b889ef14
stoken <zcyu225@163.com>
2021-03-25 17:18:21 +0800

    纪念日页面

--- miniprogram/pages/memorialDay/memorialDay.ts
+++ miniprogram/pages/memorialDay/memorialDay.ts
@@ -1,6 +1,46 @@
 // memorialDay.ts
+import Dialog from '@vant/weapp/dist/dialog/dialog';
+
+// import {formatTime} from "../../utils/util"
+// console.log('formatTime', formatTime)
 Page({
-  data: {},
+  data: {
+    slideButtons: [{
+      type: 'warn',
+      text: '删除',
+      extClass: 'test',
+    }],
+    Dialog,
+    dateList: [
+      {
+
+      }
+    ],
+    showAddDialog: false
+  },
+  slideButtonTap(e: any) {
+    console.log('slide button tap', e.detail)
+    // const { position, instance } = e.detail;
+    // switch (position) {
+    //   case 'left':
+    //   case 'cell':
+    //   case position == 'right':
+    //     Dialog.confirm({
+    //       message: '确定删除吗？',
+    //     }).then(() => {
+    //       instance.close();
+    //     });
+    //     break
+    // }
+  },
+  openAddDialog() {
+    this.setData({
+      showAddDialog: true
+    })
+  },
+  buttontap(e: any) {
+    console.log(e)
+  },
   onLoad() {
     
   }
